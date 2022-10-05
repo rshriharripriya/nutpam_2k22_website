a773987c20bce2e6e479f9ba6eac4c4f381e3e9e
rshri <rshriharripriya19@gmail.com>
2022-10-05 21:26:10 +0530

    menu

--- js/main.js
+++ js/main.js
@@ -14273,3 +14273,30 @@ $('.toggle-menu').click (function(){
   $(this).toggleClass('active');
   $('#menu').toggleClass('open');
 });
+
+function animateMenu() {
+    var menu = $('.menuBg');
+    var nav = $('.mobileNav');
+
+    if (menu.hasClass('showMenu')) { // Hide menu if it's open
+        menu.removeClass('showMenu').addClass('hideMenu');
+        nav.removeClass('fadeIn');
+    } else if (menu.hasClass('hideMenu')) { // Show menu and remove hideMenu
+        menu.removeClass('hideMenu').addClass('showMenu');
+        nav.addClass('fadeIn')
+    } else {
+        menu.addClass('showMenu'); // Initial show menu
+        nav.addClass('fadeIn');
+    }
+};
+
+$(document).ready(function() {
+    $('.hamburger').on('click', function() {
+        $(this).toggleClass('open');
+        animateMenu();
+    });
+    $('.mobileNav a').on('click', function(){
+        $('.hamburger').toggleClass('open');
+        animateMenu();
+    });
+});
